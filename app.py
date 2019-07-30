7023b589841a0265b238067f0f51e7d20541f5cc
myname21-meet <myname21@meet.mit.edu>
2019-07-30 05:16:07 -0400

    routing

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+# @app.route('/')
+# def home():
+#     return "home page" 
+
 @app.route('/')
 def home():
-    return "home page" 
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
